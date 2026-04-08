d977a77a5f3e2e27718dfe843e78fc1258d64c6e
getaudio2 <paugermaisar@hotmail.es>
2026-04-08 20:18:13 +0200

    feat: añadida y testeada la conexión a la BBDD

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const pool = require('./db/index');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -8,5 +10,16 @@ app.get('/ping', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Servidor ejecutándose en el puerto ${PORT}`);
 });
+
+async function checkDB() {
+    try {
+        const response = await pool.query('SELECT NOW()');
+        console.log('Conectado a la base de datos:', response.rows[0]);
+    } catch (error) {
+        console.error('Error conectando a la base de datos:', error);
+    }
+}
+
+checkDB();
